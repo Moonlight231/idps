efa155aa2afdc2a4995346d536aae83d1436003c
Geo Bibas <denverparole@gmail.com>
2023-11-21 03:07:48 +0800

    added dashboards and update page for doctor and pharmacy

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, flash, request, redirect, url_for, session
 from flask_wtf import FlaskForm
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
-from wtforms import StringField, SubmitField, PasswordField, DateField, SelectField, FloatField, BooleanField, ValidationError
+from wtforms import StringField, SubmitField, PasswordField, DateField, SelectField, FloatField, TimeField ,BooleanField, ValidationError
 from wtforms.validators import DataRequired, EqualTo, Length
 from wtforms.widgets import TextArea
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+
 from flask_migrate import Migrate
 from sqlalchemy import and_, or_, not_
 
@@ -44,6 +45,8 @@ def load_user(user_id):
         return Customers.query.get(int(user_id))
     if stored_session == "Doctor":
         return Doctors.query.get(int(user_id))
+    if stored_session == "Pharmacy":
+        return Pharmacies.query.get(int(user_id))
 
 #Json Thing
 @app.route('/date')
@@ -127,6 +130,31 @@ class Doctors(Users, UserMixin):
     def __repr__(self):
         return '<Name %r>' % self.name
 
+# Create Model
+class Pharmacies(Users, UserMixin):
+    account_type = db.Column(db.String(200), default="Pharmacy")
+    
+    company = db.Column(db.String(200))
+    branch = db.Column(db.String(200))
+    branch_code = db.Column(db.String(200))
+
+    store_open = db.Column(db.Time)
+    store_close = db.Column(db.Time)
+
+    contact_number = db.Column(db.String(100))
+    region = db.Column(db.String(500))
+    province = db.Column(db.String(500))
+
+    city = db.Column(db.String(500))
+    brgy = db.Column(db.String(500))
+    st_or_bldg = db.Column(db.String(500))
+
+    date_added = db.Column(db.Date, default=date.today)
+    
+    # Create a String
+    def __repr__(self):
+        return '<Name %r>' % self.name
+    
 # Create Prescription Model
 class Prescriptions(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -192,6 +220,27 @@ class UpdateDoctorForm(FlaskForm):
     prc_lic_number = StringField("PRC License Number", validators=[DataRequired()])
     s2_lic_number = StringField("S2 License Number")
     submit = SubmitField("Submit")
+
+class UpdatePharmacyForm(FlaskForm):
+    company = StringField("Company", validators=[DataRequired()])
+    branch = StringField("Branch", validators=[DataRequired()])
+    branch_code = StringField("Branch Code/ID")
+    #business hours
+    store_open = TimeField("Opening Time", validators=[DataRequired()])
+    store_close = TimeField("Closing Time", validators=[DataRequired()])
+   
+    contact_number = StringField("Contact Number", validators=[DataRequired()])
+    region = SelectField("Region", choices=["NCR","CAR","Region I","Region II","Region III","Region IV-A","MIMAROPA","Region V","Region VI","Region VII","Region VIII","Region IX","Region X","Region XI","Region XII","Region XIII","BARMM"] , validators=[DataRequired()])
+    province = StringField("Province", validators=[DataRequired()])
+
+    city = StringField("City", validators=[DataRequired()])
+    brgy = StringField("Barangay", validators=[DataRequired()])
+    st_or_bldg = StringField("Street or Building", validators=[DataRequired()])
+
+    submit = SubmitField("Submit")
+
+
+
 
 
 # Create a Name Form Class
@@ -260,6 +309,7 @@ def index():
     if form.validate_on_submit():
         customer = Customers.query.filter_by(email=form.email.data).first()
         doctor = Doctors.query.filter_by(email=form.email.data).first()
+        pharmacy = Pharmacies.query.filter_by(email=form.email.data).first()
         if customer:
             #storing session
             session["account_type"] = customer.account_type
@@ -280,13 +330,24 @@ def index():
                 return redirect(url_for('doctor_dashboard'))
             else:
                 flash("Wrong password. Try Again.", "error")
+        elif pharmacy:
+            #storing session
+            session["account_type"] = pharmacy.account_type
+            #Check the hash
+            if check_password_hash(pharmacy.password_hash, form.password.data):
+                login_user(pharmacy)
+                flash("Logged in successfully.")
+                return redirect(url_for('pharmacy_dashboard'))
+            else:
+                flash("Wrong password. Try Again.", "error")
         else:
             flash("That Email is not registered yet.")
     
     if form2.validate_on_submit():
         customer = Customers.query.filter_by(email=form2.email.data).first()
         doctors = Doctors.query.filter_by(email=form2.email.data).first()
-        if (customer is None) and (doctors is None):
+        pharmacies = Pharmacies.query.filter_by(email=form2.email.data).first()
+        if (customer is None) and (doctors is None) and (pharmacies is None):
             # Hash the password!
             hashed_pw = generate_password_hash(form2.password_hash.data, "pbkdf2")
             customer = Customers(email=form2.email.data, password_hash=hashed_pw)
@@ -368,6 +429,40 @@ def doctor_dashboard():
             return render_template("doctor_dashboard.html", form=form, name_to_update=name_to_update)
     else:
         return render_template("doctor_dashboard.html", form=form, name_to_update=name_to_update, id=id)
+    
+
+@app.route('/pharmacy/dashboard', methods=['GET', 'POST'])
+@login_required
+def pharmacy_dashboard():
+    form = UpdatePharmacyForm()
+    id = current_user.id
+    name_to_update = Pharmacies.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.company = request.form['company']
+        name_to_update.branch = request.form['branch']
+        name_to_update.branch_code = request.form['branch_code']
+
+        name_to_update.store_open = request.form['store_open']
+        name_to_update.store_close = request.form['store_close']
+
+        name_to_update.contact_number = request.form['contact_number']
+        name_to_update.region = request.form['region']
+        name_to_update.province = request.form['province']
+
+        name_to_update.city = request.form['city']
+        name_to_update.brgy = request.form['brgy']
+        name_to_update.st_or_bldg = request.form['st_or_bldg']
+
+        try:
+            db.session.commit()
+            flash("Update Successful")
+            return render_template("pharmacy_dashboard.html", form=form, name_to_update=name_to_update)
+        except:
+            flash("Error! Looks like there's a problem.")
+            return render_template("pharmacy_dashboard.html", form=form, name_to_update=name_to_update)
+    else:
+        return render_template("pharmacy_dashboard.html", form=form, name_to_update=name_to_update, id=id)
+
 
 #Create Logout function
 @app.route('/logout', methods=['GET', 'POST'])
@@ -465,6 +560,30 @@ def add_doctor():
     our_doctors = Doctors.query.order_by(Doctors.date_added)
     return render_template("add_doctor.html", form=RegisterForm(), our_doctors=our_doctors)
 
+@app.route('/pharmacy/add', methods=['get', 'post'])
+def add_pharmacy():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        doctor = Doctors.query.filter_by(email=form.email.data).first()
+        customer = Customers.query.filter_by(email=form.email.data).first()
+        pharmacy = Pharmacies.query.filter_by(email=form.email.data).first()
+        if (doctor is None) and (customer is None) and (pharmacy is None):
+            # Hash the password!
+            hashed_pw = generate_password_hash(form.password_hash.data, "pbkdf2")
+            pharmacy = Pharmacies(email=form.email.data, password_hash=hashed_pw)
+            db.session.add(pharmacy)
+            db.session.commit()
+            form.email = ''
+            form.password_hash = ''
+            form.password_hash2 = ''
+            flash("Pharmacy Added Successfully!")
+        else:
+            form.password_hash = ''
+            form.password_hash2 = ''
+            flash("That Email is already in use.")
+    our_pharmacies = Pharmacies.query.order_by(Pharmacies.date_added)
+    return render_template("add_pharmacy.html", form=RegisterForm(), our_pharmacies=our_pharmacies)
+
 
 # Create Update Database Record
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
@@ -495,6 +614,68 @@ def update(id):
             return render_template("update.html", form=form, name_to_update=name_to_update)
     else:
         return render_template("update.html", form=form, name_to_update=name_to_update, id=id)
+
+# Create Update Database Record
+@app.route('/doctor_update/<int:id>', methods=['GET', 'POST'])
+def doctor_update(id):
+    form = UpdateDoctorForm()
+    name_to_update = Doctors.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.first_name = request.form['first_name']
+        name_to_update.middle_name = request.form['middle_name']
+        name_to_update.last_name = request.form['last_name']
+        name_to_update.birthdate = request.form['birthdate']
+
+        name_to_update.ptr_number = request.form['ptr_number']
+        name_to_update.specialty = request.form['specialty']
+
+
+        name_to_update.contact_number = request.form['contact_number']
+        name_to_update.address = request.form['address']
+
+        name_to_update.prc_lic_number = request.form['prc_lic_number']
+        name_to_update.s2_lic_number = request.form['s2_lic_number']
+        
+        try:
+            db.session.commit()
+            flash("Update Successful")
+            return render_template("doctor_update.html", form=form, name_to_update=name_to_update)
+        except:
+            flash("Error! Looks like there's a problem.")
+            return render_template("doctor_update.html", form=form, name_to_update=name_to_update)
+    else:
+        return render_template("doctor_update.html", form=form, name_to_update=name_to_update, id=id)
+
+# Create Update Database Record
+@app.route('/pharmacy_update/<int:id>', methods=['GET', 'POST'])
+def pharmacy_update(id):
+    form = UpdatePharmacyForm()
+    name_to_update = Pharmacies.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.company = request.form['company']
+        name_to_update.branch = request.form['branch']
+        name_to_update.branch_code = request.form['branch_code']
+
+        name_to_update.store_open = request.form['store_open']
+        name_to_update.store_close = request.form['store_close']
+
+        name_to_update.contact_number = request.form['contact_number']
+        name_to_update.region = request.form['region']
+        name_to_update.province = request.form['province']
+
+        name_to_update.city = request.form['city']
+        name_to_update.brgy = request.form['brgy']
+        name_to_update.st_or_bldg = request.form['st_or_bldg']
+
+        try:
+            db.session.commit()
+            flash("Update Successful")
+            return render_template("pharmacy_update.html", form=form, name_to_update=name_to_update)
+        except:
+            flash("Error! Looks like there's a problem.")
+            return render_template("pharmacy_update.html", form=form, name_to_update=name_to_update)
+    else:
+        return render_template("pharmacy_update.html", form=form, name_to_update=name_to_update, id=id)
 
 
 '''# Create Update Database Record
